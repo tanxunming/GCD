033e510ce0c8c70d7890bf4942f2bb4bde54f207
谭浔明 <2514316667@qq.com>
2019-03-05 11:03:03 +0800

    Add files via upload

---
+++ iOS-GCD/iOS-GCD/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS-GCD
+//
+//  Created by xunmingtan on 2019/3/4.
+//  Copyright © 2019 xunmingtan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
